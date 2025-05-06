5547e8acd2c5b6d7a803620bbbff802810b352cb
scorbiclife <scorbiclife+git@gmail.com>
2025-05-06 11:14:43 +0900

    setup(eslint): set browser environment
    Needed to resolve eslint no-undef rule (`document` is undefined)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,14 @@ import xoConfig from "eslint-config-xo";
 import prettierConfig from "eslint-config-prettier";
 import { defineConfig } from "@eslint/config-helpers";
 
-export default defineConfig(/** @type {any} */ ([...xoConfig, prettierConfig]));
+export default defineConfig(
+  /** @type {any} */ ([
+    ...xoConfig,
+    prettierConfig,
+    {
+      env: {
+        browser: true,
+      },
+    },
+  ]),
+);
