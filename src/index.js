46dae6e5f7abc286ecdcc0ece47ee11f4895fd5e
scorbiclife <scorbiclife+git@gmail.com>
2025-05-06 11:21:57 +0900

    refactor(js): use helper function for dom creation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import "./index.css";
 
+function $(tagName, attributes = {}) {
+  return function (...children) {
+    const element = document.createElement(tagName);
+    for (const [attribute, value] of Object.entries(attributes)) {
+      element.setAttribute(attribute, value);
+    }
+    element.replaceChildren(...children);
+    return element;
+  };
+}
+
 function component() {
-  const element = document.createElement("div");
-  element.innerHTML = "Webpack Template Setup Complete";
-  return element;
+  return $("div")("Hello, Webpack!");
 }
 
 document.getElementById("content")?.appendChild(component());
